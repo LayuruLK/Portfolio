6525cc3e02d8f24009a018a1e527ed429848e8b8
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2026-02-25 11:45:44 +0530

    feat: createed navbar

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"]
+});
+
+export const metadata: Metadata = {
+  title: "Layuru Karunathilaka",
+  description: "The official web for Layuru Karunathilaka",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
